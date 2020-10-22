e9e2a7b5c467647b3dd18600763baafda9bb0189
Ramon <rfmendesbrazil@gmail.com>
2020-10-22 10:59:19 -0300

    #section-iconsprites

--- DesignArsenal/Src/App.cs
+++ DesignArsenal/Src/App.cs
@@ -61,8 +61,9 @@ namespace DesignArsenal
 			if(in_test)
 				AppHost.CallFunction("View_FocusIt", new SciterValue(true));
 
-			DirSyncer.Login();
-			DirSyncer.Sync();
+			//DirSyncer.Login();
+			//DirSyncer.Sync();
+
 #if !OSX
 			PInvokeUtils.RunMsgLoop();
 #endif
