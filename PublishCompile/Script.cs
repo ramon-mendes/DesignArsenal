15aa07deb3a98b3de13b1394dd1de41e4987e422
Ramon <rfmendesbrazil@gmail.com>
2020-10-30 14:43:53 -0300

    AutoPush

--- PublishCompile/Script.cs
+++ PublishCompile/Script.cs
@@ -97,7 +97,7 @@ partial class Script
 		else
 		{
 			string how = "Clean,Build";
-			SpawnProcess(@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe",
+			SpawnProcess(@"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe",
 				    CWD + $"..\\{APPNAME}\\{APPNAME}Windows.csproj /t:{how} /p:Configuration={CONFIG} /p:Platform=x64");
 
 			#region Pack
