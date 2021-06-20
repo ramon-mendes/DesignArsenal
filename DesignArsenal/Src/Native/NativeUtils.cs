f409857de236409b0bbe358bf8c5ab587301c31d
Ramon <rfmendesbrazil@gmail.com>
2021-06-20 14:48:07 -0300

    gogo

--- DesignArsenal/Src/Native/NativeUtils.cs
+++ DesignArsenal/Src/Native/NativeUtils.cs
@@ -55,29 +55,6 @@ namespace DesignArsenal.Native
 			return true;
 		}
 
-		#region
-		public static void PlayWav(string path)
-		{
-			PlaySound(path, new IntPtr(), PlaySoundFlags.SND_ASYNC | PlaySoundFlags.SND_FILENAME);
-		}
-
-		[DllImport("winmm.DLL", EntryPoint = "PlaySound", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
-		private static extern bool PlaySound(string szSound, IntPtr hMod, PlaySoundFlags flags);
-
-		[System.Flags]
-		public enum PlaySoundFlags : int
-		{
-			SND_SYNC = 0x0000,
-			SND_ASYNC = 0x0001,
-			SND_NODEFAULT = 0x0002,
-			SND_LOOP = 0x0008,
-			SND_NOSTOP = 0x0010,
-			SND_NOWAIT = 0x00002000,
-			SND_FILENAME = 0x00020000,
-			SND_RESOURCE = 0x00040004
-		}
-		#endregion
-
 		public static void Trace(string str)
 		{
 			 OutputDebugString(str);
