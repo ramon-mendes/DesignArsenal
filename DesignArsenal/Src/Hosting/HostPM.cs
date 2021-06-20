41cb8aaee7162d306589574735f44cb3c2cffcba
Ramon <rfmendesbrazil@gmail.com>
2021-06-20 14:33:55 -0300

    sync

---
+++ DesignArsenal/Src/Hosting/HostPM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SciterSharp;
+
+namespace DesignArsenal.Hosting
+{
+	partial class HostEvh : SciterEventHandler
+	{
+		public void Host_PlaySound(SciterValue[] args)
+		{
+			var sound = BaseHost.LoadResource(args[0].Get(""));
+			var tmp = Path.GetTempFileName() + ".wav";
+			File.WriteAllBytes(tmp, sound);
+			Native.NativeUtils.PlayWav(tmp);
+		}
+	}
+}
